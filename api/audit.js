900630147918de754347065891900aa4250505cc
Madabathula Tejo Prakash <tejohere6@gmail.com>
2026-06-25 23:23:10 +0530

    Consolidate audit route to remove nested folder

--- api/audit.js
+++ api/audit.js
@@ -1,4 +1,4 @@
-import { getPool, handleOptions, ok, err } from '../_db.js';
+import { getPool, handleOptions, ok, err } from './_db.js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
